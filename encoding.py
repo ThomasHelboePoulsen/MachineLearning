c2655ef4208964c2e998c2ac4a554bbb2b28b969
ThomasHelboePoulsen <thomaspoulsen030303@gmail.com>
2025-02-18 15:51:26 +0100

    Initial Commit

---
+++ encoding.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+wanted_columns = ['forretningsproces', 'id_lokalId', 'kommunekode', 'registreringFra', 'virkningFra', 'status', 'byg007Bygningsnummer', 'byg021BygningensAnvendelse', 'byg026Opførelsesår', 'byg027OmTilbygningsår', 'byg032YdervæggensMateriale', 'byg033Tagdækningsmateriale', 'byg037KildeTilBygningensMaterialer', 'byg038SamletBygningsareal', 'byg039BygningensSamledeBoligAreal', 'byg040BygningensSamledeErhvervsAreal', 'byg041BebyggetAreal', 'byg042ArealIndbyggetGarage', 'byg043ArealIndbyggetCarport', 'byg044ArealIndbyggetUdhus', 'byg045ArealIndbyggetUdestueEllerLign', 'byg046SamletArealAfLukkedeOverdækningerPåBygningen', 'byg047ArealAfAffaldsrumITerrænniveau', 'byg048AndetAreal', 'byg049ArealAfOverdækketAreal', 'byg050ArealÅbneOverdækningerPåBygningenSamlet', 'byg051Adgangsareal', 'byg053BygningsarealerKilde', 'byg054AntalEtager', 'byg055AfvigendeEtager', 'byg056Varmeinstallation', 'byg057Opvarmningsmiddel', 'byg058SupplerendeVarme', 'byg069Sikringsrumpladser', 'byg133KildeTilKoordinatsæt', 'byg134KvalitetAfKoordinatsæt', 'byg135SupplerendeOplysningOmKoordinatsæt', 'byg136PlaceringPåSøterritorie', 'byg404Koordinat', 'byg406Koordinatsystem']
+
+df = pd.read_excel("filterede_data.xlsx")
+print(list(df.columns))
+#df = pd.get_dummies(df,columns=["kommunekode"], dtype=int)
+#print(df)
+print(df[wanted_columns])
+df.to_excel("try_encoded.xlsx")
