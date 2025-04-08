ecd0c386af2c34ff0465eb3b18aa97e56b9ea8d7
s235218 <s235218@dtu.dk>
2025-04-08 17:42:14 +0200

    maybe works

--- regression_partb.py
+++ regression_partb.py
@@ -14,7 +14,7 @@ def fit_predict_ann(X_train,y_train, X_test,h):
                 StandardScaler(),
                 MLPRegressor(
                     hidden_layer_sizes=(h,),
-                    #activation='identity',
+                    learning_rate_init=0.01,
                     solver='adam',
                     max_iter=10000,
                     early_stopping=True,
@@ -46,7 +46,7 @@ def fit_predict_linreg(X_train,y_train, X_test,lamda):
 # Load excel file
 file_path = 'Data5_constant_columns_removed.csv'
 df = pd.read_csv(file_path)
-df = df.head(500)
+df = df.head(50)
 
 # Column names
 # print(df.columns)
@@ -77,13 +77,18 @@ K1 = 2 # Outer CV
 K2 = 2 # Inner CV
 
 # Hyperparameter values (hidden units)
-hidden_units_range = [1, 2, 4, 8, 16]
+hidden_units_range = [1, 2, 4, 8, 16,32,64,128]
 
 # Outer loop: to estimate generalisation error
 kf_outer = KFold(n_splits=K1, shuffle=True, random_state=24)
 outer_ann_test_errors = []
 outer_baseline_test_errors = []
 outer_linreg_test_errors = []
+ls = []
+hs = []
+linreg_all_errors = []
+ann_all_errors = []
+baseline_all_errors = []
 
 # Loop over outer folds
 for outer_train_idx, outer_test_idx in kf_outer.split(X):
@@ -95,6 +100,7 @@ for outer_train_idx, outer_test_idx in kf_outer.split(X):
     avg_val_ann_errors = []
     avg_val_baseline_errors = []
     avg_val_linreg_errors = []
+
 
     # Inner loop: to select the best model/hyperparameter
     kf_inner = KFold(n_splits=K2, shuffle=True, random_state=37)
@@ -114,6 +120,7 @@ for outer_train_idx, outer_test_idx in kf_outer.split(X):
         # Compute average validation error across inner folds for model M_s
         avg_val_ann_errors.append(np.mean(inner_ann_val_errors))
 
+
         #LINREG
         for l in [100,500,1000,1500]:
             inner_linreg_val_errors = []
@@ -125,29 +132,57 @@ for outer_train_idx, outer_test_idx in kf_outer.split(X):
     best_model_index_ann = np.argmin(avg_val_ann_errors)
     best_h = hidden_units_range[best_model_index_ann]
     y_best_ann = fit_predict_ann(X_par,y_par, X_test,best_h)
+    ann_all_errors.extend((y_best_ann-y_test)**2)
     test_mse_ann = mean_squared_error(y_test, y_best_ann)
     outer_ann_test_errors.append(test_mse_ann)
+    hs.append(best_h)
 
-    # Select the best model ANN M*
+    # Select the best model linreg M*
     best_model_index_linreeg = np.argmin(avg_val_linreg_errors)
     best_l = hidden_units_range[best_model_index_ann]
     y_best_linreg = fit_predict_linreg(X_par,y_par, X_test,best_l)
+    linreg_all_errors.extend((y_best_linreg-y_test)**2)
     test_mse_linreg = mean_squared_error(y_test, y_best_linreg)
     outer_linreg_test_errors.append(test_mse_linreg)
+    ls.append(l)
     #baseline
+
+    y_baseline = fit_predict_baseline(X_par,y_par, X_test)
+    baseline_all_errors.extend((y_baseline-y_test)**2)
     outer_baseline_test_errors.append(
         mean_squared_error(
             y_test,
-            fit_predict_baseline(X_par,y_par, X_test)
+            y_baseline
         )
     )
     print(f"Outer fold: Best h = {best_h}, ANN Test MSE = {test_mse_ann:.2f}")
-    print(f"Outer fold:                  , baseline Test MSE = {outer_baseline_test_errors[-1]:.2f}")
-    print(f"Outer fold:                  , baseline Test MSE = {test_mse_linreg:.2f}")
+    print(f"Outer fold: baseline Test MSE = {outer_baseline_test_errors[-1]:.2f}")
+    print(f"Outer fold: linreg Test MSE = {test_mse_linreg:.2f}")
 
 # Compute the estimate of the generalisation error, Ê_gen
 E_gen = np.mean(outer_ann_test_errors)
 print(f"Estimated generalisation error (ANN): {E_gen:.2f}")
 print(f"Estimated generalisation error (baseline): {(np.mean(outer_baseline_test_errors)):.2f}")
-print(f"Estimated generalisation error (baseline): {(np.mean(outer_linreg_test_errors)):.2f}")
+print(f"Estimated generalisation error (linreg): {(np.mean(outer_linreg_test_errors)):.2f}")
 
+
+dict_ = {
+    "h_i" : hs,
+    "E_ANN": outer_ann_test_errors,
+    "l_i": ls,
+    "E_linreg": outer_linreg_test_errors,
+    "E_baseline": outer_baseline_test_errors
+}
+results = pd.DataFrame(dict_)
+results.to_excel("project2Table1.xlsx")
+
+tTestResults = {
+    "ann": ann_all_errors,
+    "linreg": linreg_all_errors,
+    "baseline": baseline_all_errors
+}
+
+for key,val in tTestResults.items():
+    print(f"size of {key}: {len(val)}")
+
+pd.DataFrame(tTestResults).to_excel("tTestResults.xlsx")
