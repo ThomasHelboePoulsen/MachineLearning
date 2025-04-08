bd45578573e03255fa8742024258b797a7f782b6
Augersen <165222066+augersen@users.noreply.github.com>
2025-04-08 12:44:44 +0200

    created find_lambdas

---
+++ find_lambdas.py
@@ -0,0 +1,39 @@
+#Regularization
+import pandas as pd
+import numpy as np
+from sklearn.linear_model import Lasso,Ridge
+from sklearn.model_selection import cross_val_score
+import matplotlib.pyplot as plt
+
+from generalTools import set_0_column, standardize
+
+def main():
+    file = "Data5_constant_columns_removed.csv"
+    Data = set_0_column(pd.read_csv(file), "byg026Opførelsesår")
+    Data = standardize(Data)
+    attributeNames = list(pd.read_csv(file).columns)
+    attributeNames[0], attributeNames[attributeNames.index("byg026Opførelsesår")] = attributeNames[attributeNames.index("byg026Opførelsesår")], attributeNames[0]
+    
+    X = Data[:, 1:]
+    y = Data[:, 0]
+    
+    lambdas = [0.001,0.01, 0.1, 1, 10, 100, 1000, 10000]
+    results = []
+    
+    for lamb in lambdas:
+        print("Current lambda: " + str(lamb))
+        model = Ridge(alpha=lamb)
+        mse = cross_val_score(model, X, y, cv=10, scoring="neg_mean_squared_error")
+        results.append(-np.mean(mse))
+        print(mse)
+        print()
+    
+    plt.plot(lambdas, results)
+    plt.xscale('log') #Makes it easier to visualize.
+    plt.xlabel('Lambda values')
+    plt.ylabel('MSE')
+    plt.title('Regularization parameter - Ridge')   
+    plt.show()
+
+if __name__ == "__main__":
+    main()
